d6b6b9eea8031f4989bc8b585e5b29aa568ce830
Angela <angelakgo20@gmail.com>
2021-01-13 20:22:45 -0300

    docs: added tailwind, TypeScript. Style: added a navbar

---
+++ components/nav.js
@@ -0,0 +1,12 @@
+export default function Nav() {
+    return (
+        <nav className = "flex justify-between">
+            <img/>
+            <ul className = "flex p-5">
+                <li className = "px-2">About</li>
+                <li className="pr-2">Work</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+}
