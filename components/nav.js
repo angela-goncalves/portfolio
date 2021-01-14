7877c8019248bf17f0e417dde390b9eea2228eae
Angela <angelakgo20@gmail.com>
2021-01-14 16:25:56 -0300

    style:navbar with fcc instructions

--- components/nav.js
+++ components/nav.js
@@ -1,11 +1,11 @@
 export default function Nav() {
     return (
-        <nav className = "flex justify-between">
-            <img/>
-            <ul className = "flex p-5">
-                <li className = "px-2">About</li>
-                <li className="pr-2">Work</li>
-                <li>Contact</li>
+        <nav id="navbar" className="w-full flex justify-between bg-white fixed top-0 m-1">
+            <img src="svg/personal-logo.svg" alt="Angela's Logo" className="w-14"/>
+            <ul className="w-full flex justify-around self-center">
+                <a href="#about"><li>About</li></a>
+                <a href="#projects"><li>Work</li></a>
+                <a href="#contact"><li>Contact</li></a>
             </ul>
         </nav>
     )
