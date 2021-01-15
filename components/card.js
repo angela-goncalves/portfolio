66af886c3b86a209e19372d638eacf15d0e35a6e
Angela <angelakgo20@gmail.com>
2021-01-15 13:15:19 -0300

    style: added project's changes

---
+++ components/card.js
@@ -0,0 +1,9 @@
+export default function Card(){
+    return (
+    <div id="projects" >
+        <img src="#" alt="Project's image" />
+        <h2 className="project-tile">Project's Name</h2>
+        <h3>Project's Description</h3>
+    </div>
+    )
+}
